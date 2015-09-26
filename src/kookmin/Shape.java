89e442b0fa964008a4192a6fde4606f944336057
JieunJ <jejeong01@kookmin.ac.kr>
2015-09-26 16:40:13 +0900

    4

---
+++ src/kookmin/Shape.java
@@ -0,0 +1,14 @@
+package kookmin;
+
+
+public abstract class Shape {
+        protected Point center;
+
+        public Point getCenter(){ 
+        	 	  
+        		return center; 
+        } 
+        public abstract Rectangle getBounds(); 
+        public abstract void draw(Graphics g); 
+
+}
